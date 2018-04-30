61b9f69905c42ddcfc7158ab10838d079ece676d
brian <@laskow26>
2018-04-30 12:02:29 -0400

    Updating version number

--- minerchk.sh
+++ minerchk.sh
@@ -63,7 +63,7 @@ clear
 	echo
 	echo
 	header $@
-	echo "--   Miner Check beta v1.32    --"
+	echo "--   Miner Check beta v1.33    --"
 	header $@
 	echo "Enter 1 to run quick miner checks on server (Active mining on server and in /tmp)"
 	echo 
